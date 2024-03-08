6d5a240a51f02807e23d1bc462f46ec2848f5370
motbabonkhong <bthien67@gmail.com>
2024-03-08 12:26:19 +0700

    save load

---
+++ Assets/_Assets/_Scripts/DataPersistence/DataPersistenceManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DataPersistenceManager : MonoBehaviour
+{
+    [Header("File Storage Config")]
+
+    [SerializeField] private string fileName; 
+
+    public static DataPersistenceManager instance {get; private set;}
+
+    private GameData gameData;
+    private List<IDataPersistence> dataPersistencesObjects;
+    private FileDataHandler dataHandler;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Data Persistence Manager in the scene.");
+        }
+        instance = this;
+    }
+
+
+    public void Start()
+    {  
+        this.dataHandler = new FileDataHandler(Application.persistentDataPath, fileName);
+        this.dataPersistencesObjects = FindAllDataPersistenceObjects();
+        LoadGame();
+    }
+
+
+    public void NewGame()
+    {
+        this.gameData = new GameData();
+    }
+
+    public void LoadGame()
+    {
+        this.gameData = dataHandler.Load();
+        if (this.gameData == null)
+        {
+            Debug.Log("No data was found. Initializing data to defaults.");
+            NewGame();
+        }
+
+        foreach (IDataPersistence dataPersistenceObj in dataPersistencesObjects)
+        {
+            dataPersistenceObj.LoadData(gameData);
+        }
+
+    }
+
+
+
+    public void SaveGame()
+    {
+        foreach (IDataPersistence dataPersistenceObj in dataPersistencesObjects)
+        {
+            dataPersistenceObj.SaveData(ref gameData);
+        }
+
+        Debug.Log("Saved current HP = " + gameData.currentHp);
+
+        dataHandler.Save(gameData);
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveGame();
+    }
+
+    private List<IDataPersistence> FindAllDataPersistenceObjects()
+    {
+        IEnumerable<IDataPersistence> dataPersistencesObjects = FindObjectsOfType<MonoBehaviour>().OfType<IDataPersistence>();
+
+        return new List<IDataPersistence>(dataPersistencesObjects);
+    }
+}
+

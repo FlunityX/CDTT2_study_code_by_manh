b8338698a77f387499aef05b15dbaaa992a18341
DCMCD <hatanphuc3456789@gmail.com>
2024-03-11 09:31:58 +0700

    lỗi mẹ r

--- Assets/_Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Assets/_Scripts/Player/PlayerMovement.cs
@@ -13,8 +13,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] public bool isJumping ;
     [SerializeField] public bool isFalling ;
     private float jumpTimeCounter;
-    private float jumpTime=.4f;
-    private float jumpForce = 4.5f;
+    private float jumpTime=.1f;
+    private float jumpForce = 6f;
     private float increasingSpeed = 5f;
     
     public float dirX;
@@ -48,7 +48,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (isFalling || isJumping)
         {
-            transform.Translate(moveDir * moveDistance);
+            transform.Translate(moveDir * moveDistance*2);
         }
         else
         {

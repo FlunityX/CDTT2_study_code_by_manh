de52270849a96e8a5ba5396878b09edf4c476cf5
Nakkii268 <hoangphan26868@gmail.com>
2024-04-07 12:29:09 +0700

    explosion adding

--- Assets/_Assets/_Scripts/Ability/AOEDamageAbilitySO.cs
+++ Assets/_Assets/_Scripts/Ability/AOEDamageAbilitySO.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu()]
 public class AOEDamageAbilitySO : AbilitySO
 {
+    public GameObject skillPrefab;
     public override void Activate(GameObject holder)
     {
         base.Activate(holder);
-        
+
+        Instantiate(skillPrefab,holder.transform.position, Quaternion.identity); 
     }
     public override void Deactivate(GameObject holder)
     {
         base.Deactivate(holder);
-        ;
+       
+        
     }
+    
 }
---
+++ Assets/_Assets/_Scripts/Skills/Explosion.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    
+    public void DealDamage()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(gameObject.transform.position, 8f);
+        foreach(Collider2D hit in hits)
+        {
+            if(hit.CompareTag(GameConstant.ENEMY_TAG))
+            {
+                hit.GetComponent<IReceiveDamage>().ReduceHp(999);
+            }
+
+        }
+    }
+    public void DestroyPrefab()
+    {
+        Destroy(gameObject.transform.parent.gameObject);
+    }
+    
+}
